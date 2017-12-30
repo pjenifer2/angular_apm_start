5d1cd91783234ff076e68927a0fb1e61dc46e106
Patrick Jenifer <pjuofa95@gmail.com>
2017-12-29 20:26:03 -0700

    through more on components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'pm-root',
   template: 
     `<html>
       <div>
         <h1>{{pageTitle}}</h1>
+        <pm-products></pm-products>
       </div>
     </html>`
   
